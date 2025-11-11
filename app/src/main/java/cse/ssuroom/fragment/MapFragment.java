51630477362dd17b53fbeb30b1ee1d5ea1e22ff8
최종수 <chlwhdtn03@naver.com>
2025-11-11 14:23:12 +0900

    카메라 권한 요구 후 AR Core 다운로드 넘어가도록 추가

--- app/src/main/java/cse/ssuroom/fragment/MapFragment.java
+++ app/src/main/java/cse/ssuroom/fragment/MapFragment.java
@@ -1,15 +1,30 @@
 package cse.ssuroom.fragment;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.ar.core.ArCoreApk;
+import com.google.ar.core.Session;
+import com.google.ar.core.exceptions.UnavailableApkTooOldException;
+import com.google.ar.core.exceptions.UnavailableArcoreNotInstalledException;
+import com.google.ar.core.exceptions.UnavailableDeviceNotCompatibleException;
+import com.google.ar.core.exceptions.UnavailableException;
+import com.google.ar.core.exceptions.UnavailableSdkTooOldException;
+import com.google.ar.core.exceptions.UnavailableUserDeclinedInstallationException;
 import com.naver.maps.geometry.LatLng;
 import com.naver.maps.map.CameraPosition;
 import com.naver.maps.map.MapView;
@@ -32,6 +47,11 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
     private FragmentMapBinding binding;
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1000;
 
+    private String TAG = "[AR]";
+
+    private boolean isUserRequestedInstall = true;
+    private Session session;
+
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -41,8 +61,10 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
     private String mParam1;
     private String mParam2;
 
+    private ActivityResultLauncher<String> requestPermissionLauncher;
+
     public MapFragment() {
-        // Required empty public constructor
+
     }
 
     /**
@@ -80,6 +102,14 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+        requestPermissionLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+            if (isGranted) {
+                // Permission granted
+            } else {
+                Toast.makeText(getContext(), "카메라 권한이 필요합니다.", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
@@ -93,7 +123,107 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
             new FilterBottomSheet().show(getChildFragmentManager(), "filter");
         });
 
+        maybeEnableArButton();
+
         return binding.getRoot();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissionLauncher.launch(Manifest.permission.CAMERA);
+        }
+
+        try {
+            if (session == null) {
+                switch (ArCoreApk.getInstance().requestInstall(getActivity(), isUserRequestedInstall)) {
+                    case INSTALLED:
+                        // Success: Safe to create the AR session.
+                        session = new Session(getContext());
+                        break;
+                    case INSTALL_REQUESTED:
+                        // When this method returns `INSTALL_REQUESTED`:
+                        // 1. ARCore pauses this activity.
+                        // 2. ARCore prompts the user to install or update Google Play
+                        //    Services for AR (market://details?id=com.google.ar.core).
+                        // 3. ARCore downloads the latest device profile data.
+                        // 4. ARCore resumes this activity. The next invocation of
+                        //    requestInstall() will either return `INSTALLED` or throw an
+                        //    exception if the installation or update did not succeed.
+                        isUserRequestedInstall = false;
+                }
+            }
+        } catch (UnavailableUserDeclinedInstallationException e) {
+            Toast.makeText(getContext(), "TODO: handle exception " + e, Toast.LENGTH_LONG).show();
+        } catch (UnavailableDeviceNotCompatibleException e) {
+            throw new RuntimeException(e);
+        } catch (UnavailableSdkTooOldException e) {
+            throw new RuntimeException(e);
+        } catch (UnavailableArcoreNotInstalledException e) {
+            throw new RuntimeException(e);
+        } catch (UnavailableApkTooOldException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void maybeEnableArButton() {
+        ArCoreApk.getInstance().checkAvailabilityAsync(getContext(), availability -> {
+            if (availability.isSupported()) {
+                binding.ARBtn.setVisibility(View.VISIBLE);
+                binding.ARBtn.setEnabled(true);
+            } else { // The device is unsupported or unknown.
+                binding.ARBtn.setVisibility(View.INVISIBLE);
+                binding.ARBtn.setEnabled(false);
+            }
+        });
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if(session != null) {
+            session.close();
+        }
+    }
+
+    // Verify that ARCore is installed and using the current version.
+    private boolean isARCoreSupportedAndUpToDate() {
+        ArCoreApk.Availability availability = ArCoreApk.getInstance().checkAvailability(getContext());
+        switch (availability) {
+            case SUPPORTED_INSTALLED:
+                return true;
+
+            case SUPPORTED_APK_TOO_OLD:
+            case SUPPORTED_NOT_INSTALLED:
+                try {
+                    // Request ARCore installation or update if needed.
+                    ArCoreApk.InstallStatus installStatus = ArCoreApk.getInstance().requestInstall(getActivity(), true);
+                    switch (installStatus) {
+                        case INSTALL_REQUESTED:
+                            Log.i(TAG, "ARCore installation requested.");
+                            return false;
+                        case INSTALLED:
+                            return true;
+                    }
+                } catch (UnavailableException e) {
+                    Log.e(TAG, "ARCore not installed", e);
+                }
+                return false;
+
+            case UNSUPPORTED_DEVICE_NOT_CAPABLE:
+                // This device is not supported for AR.
+                return false;
+
+            case UNKNOWN_CHECKING:
+                // ARCore is checking the availability with a remote query.
+                // This function should be called again after waiting 200 ms to determine the query result.
+            case UNKNOWN_ERROR:
+            case UNKNOWN_TIMED_OUT:
+                // There was an error checking for AR availability. This may be due to the device being offline.
+                // Handle the error appropriately.
+        }
+        return false;
     }
 
     @Override
