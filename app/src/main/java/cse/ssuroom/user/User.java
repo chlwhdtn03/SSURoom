ad0b9ce1de4bd4469d3860c296132ada3e5dd486
Dalzoo <eastman36@naver.com>
2025-11-12 17:29:35 +0900

    User Class 작성 + 이미지 불러오기를 위한 Glide Api 적용 (Gradle) 완료 RegisterFragment에서 바로 사용자 정보를 Firestore에 저장 완료 회원가입 성공 시 유저 정보 Firestore에 저장되는 거 확인 완료 Firebase storage 의존성 추가 갤러리 접근 권한 설정 프로필 사진 갤러리에서 변경 가능 완료 Firebase에 Uid에 맞게 프로필 사진 업로드도 확인.

---
+++ app/src/main/java/cse/ssuroom/user/User.java
@@ -0,0 +1,52 @@
+package cse.ssuroom.user;
+
+// FireStore에 저장할 User Class (프로필 이미지는 생성자로 바로 불가능함)
+public class User {    private String uid;
+    private String name;
+    private String email;
+    private String profileImageUrl; // 프로필 이미지 URL을 저장할 필드
+
+    public User() {
+    }
+
+    // 데이터를 저장할 때 사용할 생성자
+    public User(String uid, String name, String email) {
+        this.uid = uid;
+        this.name = name;
+        this.email = email;
+        this.profileImageUrl = ""; // 처음에는 빈 값으로 초기화
+    }
+
+    // Getter와 Setter
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getProfileImageUrl() {
+        return profileImageUrl;
+    }
+
+    public void setProfileImageUrl(String profileImageUrl) {
+        this.profileImageUrl = profileImageUrl;
+    }
+}
